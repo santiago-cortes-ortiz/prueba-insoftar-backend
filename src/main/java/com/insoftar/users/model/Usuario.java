8ed77b15e97525019a605f805916207239f9e4cf
santiago cortes <jeissoncortes02@outlook.com>
2021-07-23 12:37:44 -0500

    capa de controlador esta completa

--- src/main/java/com/insoftar/users/model/Usuario.java
+++ src/main/java/com/insoftar/users/model/Usuario.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
 public class Usuario implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
